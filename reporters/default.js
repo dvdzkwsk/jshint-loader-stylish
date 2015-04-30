38fe16b660a3da4fa0260de19b1513f30f444e70
David Zukowski <zukoweb@gmail.com>
2015-04-30 15:15:42 -0400

    Hotfix: reporter styles now properly export a function for jshint-loader.

--- reporters/default.js
+++ reporters/default.js
@@ -1,21 +1,23 @@
 var chalk = require('chalk');
 
-module.exports = function (errors, config) {
-  if (!errors) return;
+module.exports = function (config) {
+  return function (errors) {
+    if (!errors) return;
 
-  errors.forEach(function (err) {
-    var isError;
-    if (!err) return;
+    errors.forEach(function (err) {
+      var isError;
+      if (!err) return;
 
-    isError = err.code && err.code[0] === 'E';
-    console.log(
-      ' ',
-      chalk.gray('line ' + err.line),
-      chalk.gray('col ' + err.character),
-      chalk.gray(':: ' + err.evidence.trim())
-    );
-    console.log(
-      isError ? chalk.red(err.reason) : chalkBlue(err.reason), '\n'
-    );
-  });
+      isError = err.code && err.code[0] === 'E';
+      console.log(
+        ' ',
+        chalk.gray('line ' + err.line),
+        chalk.gray('col ' + err.character),
+        chalk.gray(':: ' + err.evidence.trim())
+      );
+      console.log(
+        isError ? chalk.red(err.reason) : chalkBlue(err.reason), '\n'
+      );
+    });
+  };
 };
--- reporters/true-stylish.js
+++ reporters/true-stylish.js
@@ -1,26 +1,28 @@
 var chalk = require('chalk'),
     table = require('text-table');
 
-module.exports = function (errors, config) {
-  if (!errors) return;
+module.exports = function (config) {
+  return function (errors) {
+    if (!errors) return;
 
-  var rows = errors.map(function (err) {
-    var isError, row;
-    if (!err) return;
+    var rows = errors.map(function (err) {
+      var isError, row;
+      if (!err) return;
 
-    isError = err.code[0] === 'E';
-    row = [
-      '',
-      chalk.gray('line ' + err.line),
-      chalk.gray('col ' + err.character),
-      isError ? chalk.red(err.reason) : chalkBlue(err.reason)
-    ];
+      isError = err.code[0] === 'E';
+      row = [
+        '',
+        chalk.gray('line ' + err.line),
+        chalk.gray('col ' + err.character),
+        isError ? chalk.red(err.reason) : chalkBlue(err.reason)
+      ];
 
-    if (config.verbose) {
-      row.push(chalk.gray('(' + err.code + ')'));
-    }
-    return row;
-  });
+      if (config.verbose) {
+        row.push(chalk.gray('(' + err.code + ')'));
+      }
+      return row;
+    });
 
-  console.log(table(rows));
+    console.log(table(rows));
+  };
 };
